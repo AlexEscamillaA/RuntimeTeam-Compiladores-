69c95aca1b971fbfe755608ccefb5050d7250aee
MelancholistLion <leopuente@hotmail.com>
2024-02-14 03:45:49 -0600

    analizadorLexicoPrimeraRevision

--- Runtime Compiladores Proyecto/Form1.Designer.cs
+++ Runtime Compiladores Proyecto/Form1.Designer.cs
@@ -28,12 +28,119 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.label1 = new System.Windows.Forms.Label();
+            this.expresionRegularLabel = new System.Windows.Forms.Label();
+            this.expresionPostfijaLabel = new System.Windows.Forms.Label();
+            this.analizadorLexicoLabel = new System.Windows.Forms.Label();
+            this.expRegularLabel = new System.Windows.Forms.Label();
+            this.expPostfijaLabel = new System.Windows.Forms.Label();
+            this.expresionRegularResponse = new System.Windows.Forms.TextBox();
+            this.expresionPostfijaResponse = new System.Windows.Forms.TextBox();
+            this.conversionButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // expresionRegularLabel
+            // 
+            this.expresionRegularLabel.Location = new System.Drawing.Point(0, 0);
+            this.expresionRegularLabel.Name = "expresionRegularLabel";
+            this.expresionRegularLabel.Size = new System.Drawing.Size(100, 23);
+            this.expresionRegularLabel.TabIndex = 0;
+            // 
+            // expresionPostfijaLabel
+            // 
+            this.expresionPostfijaLabel.Location = new System.Drawing.Point(0, 0);
+            this.expresionPostfijaLabel.Name = "expresionPostfijaLabel";
+            this.expresionPostfijaLabel.Size = new System.Drawing.Size(100, 23);
+            this.expresionPostfijaLabel.TabIndex = 0;
+            // 
+            // analizadorLexicoLabel
+            // 
+            this.analizadorLexicoLabel.AutoSize = true;
+            this.analizadorLexicoLabel.Location = new System.Drawing.Point(60, 9);
+            this.analizadorLexicoLabel.Name = "analizadorLexicoLabel";
+            this.analizadorLexicoLabel.Size = new System.Drawing.Size(127, 20);
+            this.analizadorLexicoLabel.TabIndex = 0;
+            this.analizadorLexicoLabel.Text = "Analizador Léxico";
+            // 
+            // expRegularLabel
+            // 
+            this.expRegularLabel.AutoSize = true;
+            this.expRegularLabel.Location = new System.Drawing.Point(11, 44);
+            this.expRegularLabel.Name = "expRegularLabel";
+            this.expRegularLabel.Size = new System.Drawing.Size(128, 20);
+            this.expRegularLabel.TabIndex = 1;
+            this.expRegularLabel.Text = "Expresión Regular";
+            // 
+            // expPostfijaLabel
+            // 
+            this.expPostfijaLabel.AutoSize = true;
+            this.expPostfijaLabel.Location = new System.Drawing.Point(11, 160);
+            this.expPostfijaLabel.Name = "expPostfijaLabel";
+            this.expPostfijaLabel.Size = new System.Drawing.Size(125, 20);
+            this.expPostfijaLabel.TabIndex = 2;
+            this.expPostfijaLabel.Text = "Expresión Postfija";
+            // 
+            // expresionRegularResponse
+            // 
+            this.expresionRegularResponse.Location = new System.Drawing.Point(14, 67);
+            this.expresionRegularResponse.Name = "expresionRegularResponse";
+            this.expresionRegularResponse.Size = new System.Drawing.Size(329, 27);
+            this.expresionRegularResponse.TabIndex = 3;
+            // 
+            // expresionPostfijaResponse
+            // 
+            this.expresionPostfijaResponse.Location = new System.Drawing.Point(11, 183);
+            this.expresionPostfijaResponse.Name = "expresionPostfijaResponse";
+            this.expresionPostfijaResponse.Size = new System.Drawing.Size(332, 27);
+            this.expresionPostfijaResponse.TabIndex = 4;
+            // 
+            // conversionButton
+            // 
+            this.conversionButton.Location = new System.Drawing.Point(126, 109);
+            this.conversionButton.Name = "conversionButton";
+            this.conversionButton.Size = new System.Drawing.Size(217, 29);
+            this.conversionButton.TabIndex = 5;
+            this.conversionButton.Text = "Expresión Regular a Postfija";
+            this.conversionButton.UseVisualStyleBackColor = true;
+            this.conversionButton.Click += new System.EventHandler(this.conversionButton_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1096, 477);
+            this.Controls.Add(this.conversionButton);
+            this.Controls.Add(this.expresionPostfijaResponse);
+            this.Controls.Add(this.expresionRegularResponse);
+            this.Controls.Add(this.expPostfijaLabel);
+            this.Controls.Add(this.expRegularLabel);
+            this.Controls.Add(this.analizadorLexicoLabel);
+            this.Name = "Form1";
+            this.Text = "Compilador";
+            this.Load += new System.EventHandler(this.Compilador_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label expresionRegularLabel;
+        private Label expresionPostfijaLabel;
+        private TextBox expresionRegular;
+        private TextBox expresionPostfija;
+        private Button boton_calcular_posfija;
+        private Label analizadorLexicoLabel;
+        private Label expRegularLabel;
+        private Label expPostfijaLabel;
+        private TextBox expresionRegularResponse;
+        private TextBox expresionPostfijaResponse;
+        private Button conversionButton;
     }
 }
--- Runtime Compiladores Proyecto/Form1.cs
+++ Runtime Compiladores Proyecto/Form1.cs
@@ -6,5 +6,193 @@ namespace Runtime_Compiladores_Proyecto
         {
             InitializeComponent();
         }
+
+        private void expresionRegular_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void boton_calcular_posfija_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Compilador_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void conversionButton_Click(object sender, EventArgs e)
+        {
+            string expresionRegular = expresionRegularResponse.Text;
+            string expresionExplicita = convierteExplicita(expresionRegular);
+            string expresionPostfija = ConvertirAPosfija(expresionExplicita);
+            expresionPostfijaResponse.Text = expresionPostfija;
+        }
+
+        public static string ConvertirAPosfija(string expresionRegular)
+        {
+            string expresionPosfija = "";
+            Stack<char> pilaOperadores = new Stack<char>(); // Inicializa la pila
+
+            foreach (char caracter in expresionRegular)
+            {
+                switch (caracter)
+                {
+                    case '(': // Si es un inicio de paréntesis
+                        pilaOperadores.Push(caracter); // Lo mete a la pila
+                        break;
+                    case ')': // Si es el final del paréntesis
+                        while (pilaOperadores.Count > 0 && pilaOperadores.Peek() != '(') // Extrae todo lo que había en el paréntesis
+                            expresionPosfija += pilaOperadores.Pop(); // Lo extraído lo mete a la expresión resultado (posfija)
+                        pilaOperadores.Pop();
+                        break;
+                    default: // Si es un número, letra u operador
+                        if (Char.IsLetterOrDigit(caracter)) // Si es un numero o una letra
+                            expresionPosfija += caracter; // Lo mete a la expresión resultado
+                        else if (Jerarquia(caracter) != 0)
+                        {
+                            bool band = true;
+                            while (band)
+                            {
+                                // Diferentes condiciones de operadores donde:
+                                // 1. No hay nada en la pila 2. El operador es el inicio de un paréntesis 3. Si la jerarquía del caracter iterado es mayor a la jerarquía del pico de la pila
+                                if (pilaOperadores.Count == 0 || pilaOperadores.Peek() == '(' || Jerarquia(caracter) > Jerarquia(pilaOperadores.Peek()))
+                                {
+                                    pilaOperadores.Push(caracter); // Mete el caracter a la pila y cambia la banda
+                                    band = false;
+                                }
+                                else
+                                    expresionPosfija += pilaOperadores.Pop(); // Sino, solamente mete el operador pico de la pila a la expresión resultado
+                            }
+                        }
+                        break;
+                }
+            }
+
+            while (pilaOperadores.Count > 0) // Si quedan operadores en la pila
+            {
+                expresionPosfija += pilaOperadores.Pop(); // Los operadores que quedan se meten a la expresión posfija
+            }
+
+            return expresionPosfija.ToString(); // Se retorna la expresión hecha
+        }
+
+        public static string ExpansionAlternativas(string alternativas)
+        {
+            string resultado = "";
+            int caracterInicial = (int)alternativas[0];
+            int caracterFinal = (int)alternativas[2]; // a - z <--- Casteamos a Int para obtener el valor en ASCII
+
+            // Agregamos las variables de un cierto rango [XX-XX] ya sean números o letras
+            for (int caracter = caracterInicial; caracter <= caracterFinal; caracter++)
+            {
+                resultado += (char)caracter; // Se devuelve el entero a carácter y se añade
+                if (caracter != caracterFinal) // Se pone la alternativa solo sino es el final
+                    resultado += '|';
+            }
+            return '(' + resultado + ')'; // Se ponen las alternativas en un string con paréntesis
+        }
+
+        public static string SeleccionAlternativas(string cadena)
+        {
+            string resultado = "";
+
+            if (cadena[0] == '[') // Si tiene corchete
+            {
+                resultado = cadena.Split('[', ']')[1]; // Deja las letras quitando los corchetes
+                resultado = string.Join('|', resultado.ToList()); // Agrega la selección de alternativas en medio de las letras
+            }
+            else if (cadena[0] == '(') // Si tiene paréntesis
+            {
+                resultado = cadena.Split('(', ')')[1]; // Deja las letras quitando los paréntesis
+                resultado = string.Join('&', resultado.ToList()); // Agrega la selección 
+            }
+
+            return '(' + resultado + ')'; // Agrega los paréntesis de inicio y fin
+        }
+
+        public static string convierteExplicita(string expresionRegular)
+        {
+            string expresionExplicita = "";
+
+            for (int posicion = 0; posicion < expresionRegular.Length; posicion++)
+            {
+                if (expresionRegular[posicion] == '[') // Si encuentra un corchete inicial
+                {
+                    // Subcadena que toma lo que hay dentro de los corchetes
+                    string cadenaSeleccionAlt = expresionRegular.Substring(posicion + 1, expresionRegular.Substring(posicion).IndexOf(']') - 1);
+
+                    if (expresionExplicita != "" && expresionExplicita.Last() == ')') // Si la cadena no está vacía y hay un paréntesis al final
+                        expresionExplicita += '&'; // Se le agrega un paréntesis
+                    if (cadenaSeleccionAlt.Contains('-')) // Si contiene un - 
+                        expresionExplicita += ExpansionAlternativas(cadenaSeleccionAlt); // Se agregan todas las alternativas
+                    else
+                        expresionExplicita += SeleccionAlternativas('[' + cadenaSeleccionAlt + ']'); // Se agregan las concatenaciones
+                    posicion += cadenaSeleccionAlt.Length + 1;
+
+                }
+                else if (expresionRegular[posicion] == '(') // Si encuentra un corchete inicial
+                {
+                    // Subcadena que toma lo que hay dentro de los paréntesis
+                    string cadenaSeleccionAlt = expresionRegular.Substring(posicion + 1, expresionRegular.Substring(posicion).IndexOf(')') - 1);
+
+                    if (expresionExplicita != "" && expresionExplicita.Last() == ')') // Si la cadena no está vacía y hay un paréntesis al final
+                        expresionExplicita += '&';
+                    // En caso de contener operandos, corchete o paréntesis
+                    if (cadenaSeleccionAlt.Contains('|') ||
+                       cadenaSeleccionAlt.Contains('[') ||
+                       cadenaSeleccionAlt.Contains('*') ||
+                       cadenaSeleccionAlt.Contains('+') ||
+                       cadenaSeleccionAlt.Contains('?') ||
+                       cadenaSeleccionAlt.Contains('('))
+                    {
+                        // Se toma la subcadena para convertirla en explícita mediante una llamada recursiva
+                        expresionExplicita += '(' + convierteExplicita(cadenaSeleccionAlt) + ')';
+                    }
+                    else
+                    {
+                        expresionExplicita += SeleccionAlternativas('(' + cadenaSeleccionAlt + ')'); // Solo se agregan las concatenaciones
+                    }
+                    posicion += cadenaSeleccionAlt.Length + 1; // Le suma el 1 al iterador que es después del final del paréntesis
+                }
+                // Condiciones para concatenación
+                else if (posicion != expresionRegular.Length - 1) // Sino ha llegado al final de la expresión
+                {
+                    // Si hay un operador y el siguiente no es una alternativa
+                    if ((expresionRegular[posicion] == '?' && expresionRegular[posicion + 1] != '|') ||
+                        (expresionRegular[posicion] == '+' && expresionRegular[posicion + 1] != '|') ||
+                        (expresionRegular[posicion] == '*' && expresionRegular[posicion + 1] != '|') ||
+                        Char.IsLetter(expresionRegular[posicion]) &&
+                        (Char.IsLetter(expresionRegular[posicion + 1]) ||
+                        expresionRegular[posicion + 1] == '(' ||
+                        expresionRegular[posicion + 1] == '['))
+                    {
+                        expresionExplicita += expresionRegular[posicion];
+                        expresionExplicita += '&';
+                    }
+                    // Sino solo agrega la concatenación
+                    else
+                        expresionExplicita += expresionRegular[posicion];
+                }
+                else
+                    expresionExplicita += expresionRegular[posicion];
+            }
+            return expresionExplicita;
+        }
+
+        // Jerarquia de Operaciones
+        public static int Jerarquia(char operador)
+        {
+            if (operador == '?' || operador == '*' || operador == '+')
+                return 3;
+            else if (operador == '&')
+                return 2;
+            else if (operador == '|')
+                return 1;
+            else return 0;
+        }
+
+        
     }
 }
